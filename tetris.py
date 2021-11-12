d1a83994a7021d5893cc8e32b60d57d5f1a678b1
andrew <andrewshimel@gmail.com>
2021-11-12 12:43:40 -0600

    fixed music issues

--- tetris.py
+++ tetris.py
@@ -160,7 +160,7 @@ counter = 0
 oldFigure = None
 tempFigure = None
 pressing_down = False
-pygame.mixer.music.load('RamRanch.wav')
+pygame.mixer.music.load('music.wav')
 pygame.mixer.music.play(-1)
 while not done:
     if game.figure is None:
